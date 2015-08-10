de131660bdfed26e7263191526650d414fd67a1b
Mandar <01.mandar@gmail.com>
2015-08-10 22:56:29 +0530

    Added helloWorld for Selenium

---
+++ 1_GooglePage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Internal;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace _1_GooglePage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirefoxDriver firefox = new FirefoxDriver();
+            firefox.Navigate().GoToUrl("https://www.google.com");
+
+            IWebElement element = firefox.FindElement(By.Name("q"));
+            element.SendKeys("mandarsblog.wordpress.com");
+            element.SendKeys(Keys.ArrowDown);
+            element.Submit();
+
+        }
+
+    }
+}
---
+++ selenium/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("https://www.youtube.com");
+        }
+    }
+}
